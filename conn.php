2e9e104fa118abc52960d310f5029f3cf53850f6
Rishi <rishi772001@gmail.com>
2021-01-26 14:48:23 +0530

    removed inline css and orgganized files

---
+++ conn.php
@@ -0,0 +1,4 @@
+<?php
+$db = mysqli_connect('localhost:33', 'root', '', 'post');
+
+?>
--- login1.php
+++ login1.php
@@ -4,90 +4,21 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Registration system PHP and MySQL</title>
-    <style>
-    * {
-        margin: 0px;
-        padding: 0px;
-    }
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script>
+    $(document).ready(function() {
+        $("#nav-placeholder").load("nav.html");
+    });
+    </script>
+    <link rel="stylesheet" href="css/login.css" />
 
-    body {
-        font-size: 120%;
-        background: #F8F8FF;
-    }
-
-    .header {
-        width: 30%;
-        margin: 50px auto 0px;
-        color: white;
-        background: #5F9EA0;
-        text-align: center;
-        border: 1px solid #B0C4DE;
-        border-bottom: none;
-        border-radius: 10px 10px 0px 0px;
-        padding: 20px;
-    }
-
-    form,
-    .content {
-        width: 30%;
-        margin: 0px auto;
-        padding: 20px;
-        border: 1px solid #B0C4DE;
-        background: white;
-        border-radius: 0px 0px 10px 10px;
-    }
-
-    .input-group {
-        margin: 10px 0px 10px 0px;
-    }
-
-    .input-group label {
-        display: block;
-        text-align: left;
-        margin: 3px;
-    }
-
-    .input-group input {
-        height: 30px;
-        width: 93%;
-        padding: 5px 10px;
-        font-size: 16px;
-        border-radius: 5px;
-        border: 1px solid gray;
-    }
-
-    .btn {
-        padding: 10px;
-        font-size: 15px;
-        color: white;
-        background: #5F9EA0;
-        border: none;
-        border-radius: 5px;
-    }
-
-    .error {
-        width: 92%;
-        margin: 0px auto;
-        padding: 10px;
-        border: 1px solid #a94442;
-        color: #a94442;
-        background: #f2dede;
-        border-radius: 5px;
-        text-align: left;
-    }
-
-    .success {
-        color: #3c763d;
-        background: #dff0d8;
-        border: 1px solid #3c763d;
-        margin-bottom: 20px;
-    }
-    </style>
 </head>
 
 <body>
-
+    <!-- admin login page -->
+    <div id="nav-placeholder"></div>
     <div class="header">
         <h2>Login</h2>
     </div>
@@ -106,9 +37,6 @@
         </div>
         <div class="input-group">
             <button type="submit" class="btn" name="login_user">Login</button>
-        </div>
-        <div class="input-group">
-            <a href="http://localhost/post/index.html">Home</a>
         </div>
 
     </form>
