f2f8f13522c2ac8f639b76d6444b5fc8157ab8b7
rishi772001 <rishi772001@gmail.com>
2021-07-23 11:46:52 +0530

    Initial Commit

---
+++ Backend-Java-Spring-Boot/src/main/java/com/project/backendjavaspringboot/BackendJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.project.backendjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendJavaSpringBootApplication.class, args);
+    }
+
+}
---
+++ Backend-Java-Spring-Boot/src/test/java/com/project/backendjavaspringboot/BackendJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.backendjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
